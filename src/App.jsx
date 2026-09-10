c902899bc9cc8032cfed19ae4ed697b212401c95
Nic Ledoux <97629165+NicLedoux@users.noreply.github.com>
2026-09-10 17:49:45 +0000

    chore: restore deleted files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { PostList } from './components/PostList.jsx'
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
+export function App() {
+  return <PostList posts={posts} />
+}
